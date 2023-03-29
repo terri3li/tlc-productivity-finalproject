e08fa89c7678f2f0118355a6c3c2e4e8fd754f6b
TerriLynn Chaffey <terrilynnchaffey@gmail.com>
2023-03-29 14:12:34 -0230

    Frontend & Backend Setup

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const express = require("express");
+const morgan = require("morgan");
+const port = 8880;
+
+express()
+  .use(morgan("tiny"))
+  .use(express.json())
+
+  // Any requests for static files will go into the public folder
+  // probably won't need
+  .use(express.static("public"))
+
+  .get("/test", (req, res) => {
+    res.status(200).json({
+      message: "Helllllo",
+    });
+  })
+
+  // catch all
+  .get("*", (req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: "This is obviously not what you are looking for.",
+    });
+  })
+
+  // Node spins up our server and sets it to listen on port 8000.
+  .listen(port, () => console.log(`Listening on port ${port}`));
---
+++ frontend/src/components/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1>Hi Hello</h1>
+    </>
+  );
+};
+
+export default App;
